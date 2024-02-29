8c02c9265f4f4963e4b2dabe5acd4b5058be488a
Alex Graf <Alex.Graf@ebp.ch>
2024-02-29 11:41:16 +0100

    clean-up

--- images/bulk-ocr/flask/readme.md
+++
@@ -1,5 +0,0 @@
-`apt-get install ocrmypdf`
-
-`pip install -r ./requirements.txt`
-
-ev. Hilfe mit poppler: https://github.com/Belval/pdf2image
--- tools/github/export-issues.sh
+++
@@ -1,3 +0,0 @@
-MAX_NUM=`gh issue list --repo Lambda-IT/asset-swissgeol-ch --limit 1 --json number \
-    | jq ".[].number"`; for n in `seq 1 $MAX_NUM`; do gh issue view $n --repo Lambda-IT/asset-swissgeol-ch \
-     --json assignees,author,body,closed,closedAt,comments,createdAt,id,labels,milestone,number,projectCards,reactionGroups,state,title,updatedAt,url >> github-dump.json; done
