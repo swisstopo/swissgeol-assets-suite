3ec12ab67e921f75a83608e306d96aabcb4089e6
till_schuetze <till.schuetze@ebp.ch>
2024-04-09 16:51:57 +0200

    assets-7: #7 remove environment for int and prod in frontend

--- apps/client-asset-sg/src/environments/environment.prod.ts
+++ apps/client-asset-sg/src/environments/environment.prod.ts
@@ -2,10 +2,4 @@ import { CompileTimeEnvironment } from './environment-type';
 
 export const environment: CompileTimeEnvironment = {
     ngrxStoreLoggerEnabled: false,
-    oauth_issuer: 'https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_1NcmOhPt4',
-    oauth_clientId: '1q6gcb279tb26bg7odpn78856p',
-    oauth_scope: 'email openid profile',
-    oauth_responseType: 'code',
-    oauth_showDebugInformation: false,
-    oauth_tokenEndpoint: 'https://ngm-prod.auth.eu-west-1.amazoncognito.com/oauth2/token',
 };
