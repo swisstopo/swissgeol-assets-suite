5035bea8a57fc7889820d4c1c1af95e0eec482d4
Alex Graf <Alex.Graf@ebp.ch>
2024-04-09 17:02:49 +0200

    #11 fix sonar-issues, unit-tests

---
+++ libs/favourite/src/lib/services/favourite.service.spec.ts
@@ -0,0 +1,40 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { Favourite, FavouriteService } from './favourite.service';
+
+describe('FavouriteService', () => {
+  let service: FavouriteService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [FavouriteService]
+    });
+
+    service = TestBed.inject(FavouriteService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify(); // Ensure that there are no outstanding requests
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should retrieve favourites from API via GET', () => {
+    const dummyFavourites: Favourite[] = [{}, {}, {}];
+
+    service.getFavourites().subscribe(favourites => {
+      expect(favourites.length).toBe(3);
+      expect(favourites).toEqual(dummyFavourites);
+    });
+
+    const request = httpMock.expectOne(`/api/user/favourite`);
+    expect(request.request.method).toBe('GET');
+    request.flush(dummyFavourites);
+  });
+});
