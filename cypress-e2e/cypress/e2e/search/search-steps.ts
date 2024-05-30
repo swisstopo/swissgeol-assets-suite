c74723359cb315685103dae40ad616e99e964617
Dmytro Bykov <dmytro.bykov@ebp.ch>
2024-05-30 13:56:46 +0200

    adjusted test

--- cypress-e2e/cypress/e2e/search/search-steps.ts
+++ cypress-e2e/cypress/e2e/search/search-steps.ts
@@ -1,8 +1,9 @@
 import {Then, When} from '@badeball/cypress-cucumber-preprocessor';
 
 Then(/^The user should see a test information in resutls$/, () => {
-  cy.get('asset-sg-animate-number:contains("0")').should('not.exist');
+  cy.get('asset-sg-animate-number:contains("0")', {timeout: 3000}).should('not.exist');
   cy.get('.asset-body').should('exist');
+  cy.get('.search-results div:contains("test")').should('exist');
 });
 When(/^The user clicks enter$/, () => {
   cy.get('.search-input').type('{enter}');
