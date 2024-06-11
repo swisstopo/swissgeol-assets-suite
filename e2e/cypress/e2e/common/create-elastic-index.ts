fed21b997f78d22670e161c85e812a627e45976f
Dmytro Bykov <dmytro.bykov@ebp.ch>
2024-06-11 16:09:14 +0200

    refactoring, added test data init

--- e2e/cypress/e2e/common/create-elastic-index.ts
+++ e2e/cypress/e2e/common/create-elastic-index.ts
@@ -1,14 +1,14 @@
 import { Then, When } from '@badeball/cypress-cucumber-preprocessor';
 
-Then(/^The user should see the asset in results$/, () => {
+Then(/^The user should see the sync progress$/, () => {
   cy.get('asset-sg-animate-number:contains("0")', {timeout: 3000}).should('not.exist');
   cy.get('.asset-body').should('exist');
   cy.get('.search-results div:contains("CypressTestAsset")').should('exist');
 });
-When(/^The user clicks enter$/, () => {
+When(/^The user clicks start sync button$/, () => {
   cy.get('.search-input').type('{enter}');
 });
-When(/^A user types (.*) into the search field$/, (searchText: string) => {
+When(/^A user clicks administration menu button$/, (searchText: string) => {
   cy.get('.search-input').type(searchText);
   cy.wait(1000);
 });
--- e2e/cypress/e2e/search.feature
+++ e2e/cypress/e2e/search.feature
@@ -2,8 +2,10 @@ Feature: search
 
   Background:
     Given The user is logged in
+    And Test asset is created
+    And Elastic index is created
 
-  Scenario: Search for test information
-    When A user types information into the search field
+  Scenario: Search for test asset
+    When A user types CypressTestAsset into the search field
     And The user clicks enter
     Then The user should see a test information in results
