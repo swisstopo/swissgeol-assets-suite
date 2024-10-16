23e52cdce3ee0a456849fc021980cf52e926ec9f
Daniel von Atzigen <daniel@vatz.ch>
2024-10-16 08:07:43 +0200

    Remove unused inputs

--- apps/client-asset-sg/src/app/components/app-bar/app-bar.component.ts
+++ apps/client-asset-sg/src/app/components/app-bar/app-bar.component.ts
@@ -1,26 +1,16 @@
-import { ENTER } from '@angular/cdk/keycodes';
 import { HttpClient } from '@angular/common/http';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  inject,
-  Input,
-  OnInit,
-  Output,
-  ViewChild,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { AuthService } from '@asset-sg/auth';
 import { appSharedStateActions, fromAppShared, supportedLangs } from '@asset-sg/client-shared';
 import { isNotNull, isTruthy } from '@asset-sg/core';
 import { Lang } from '@asset-sg/shared';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { UntilDestroy } from '@ngneat/until-destroy';
 import { Store } from '@ngrx/store';
 import { flow, pipe } from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
 import queryString from 'query-string';
-import { debounceTime, EMPTY, filter, map, Observable, startWith, Subject, switchMap } from 'rxjs';
+import { debounceTime, filter, map, startWith } from 'rxjs';
 import { AppState } from '../../state/app-state';
 import { Version } from './version';
 
@@ -31,14 +21,9 @@ import { Version } from './version';
   styleUrls: ['./app-bar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppBarComponent implements OnInit {
-  // TODO use new pattern here
-  @Input() currentSearchText$: Observable<O.Option<string>> = EMPTY;
-  @Output() public searchTextChanged: Observable<O.Option<string>>;
-
-  @ViewChild('searchInput', { read: ElementRef, static: true }) searchInput!: ElementRef<HTMLInputElement>;
-
-  public readonly searchTextKeyDown$ = new Subject<KeyboardEvent>();
+export class AppBarComponent {
+  @ViewChild('searchInput', { read: ElementRef, static: true })
+  searchInput!: ElementRef<HTMLInputElement>;
 
   public version = '';
 
@@ -83,35 +68,13 @@ export class AppBarComponent implements OnInit {
     )
   );
 
-  private readonly _ngOnInit$ = new Subject<void>();
-
   constructor(private readonly router: Router, private readonly httpClient: HttpClient) {
     this.httpClient.get<Version>('/assets/version.json').subscribe((v) => (this.version = v.version));
-    this.searchTextChanged = this.searchTextKeyDown$.pipe(
-      filter((ev) => ev.keyCode === ENTER),
-      map((ev) => {
-        const value = (ev.target as HTMLInputElement).value;
-        return value ? O.some(value) : O.none;
-      })
-    );
-    // TODO use new pattern here
-    this._ngOnInit$
-      .pipe(switchMap(() => this.currentSearchText$.pipe(map(O.toNullable), untilDestroyed(this))))
-      .subscribe((currentSearchText) => {
-        if (currentSearchText != this.searchInput.nativeElement.value) {
-          this.searchInput.nativeElement.value = currentSearchText || '';
-        }
-      });
-  }
-
-  // TODO use new pattern here
-  ngOnInit() {
-    this._ngOnInit$.next();
   }
 
   logout(): void {
     this.authService.logOut();
     this.store.dispatch(appSharedStateActions.logout());
-    this.router.navigate(['/']);
+    this.router.navigate(['/']).then();
   }
 }
