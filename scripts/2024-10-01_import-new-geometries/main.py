bdebde7ee769b87a4c5733ab2f80a047698b7a01
Jannic Veith <jannic.veith@ebp.ch>
2024-11-19 16:25:04 +0100

    Add script to import geometries

--- scripts/2024-10-01_import-new-geometries/main.py
+++ scripts/2024-10-01_import-new-geometries/main.py
@@ -5,6 +5,8 @@ import numpy as np
 from geopandas import GeoDataFrame
 import os
 
+from shapely import Polygon
+
 # The following parameters need to be adjusted to the correct values:
 filedir = ''
 filename = 'geometries.gpkg'
@@ -30,6 +32,11 @@ def write_insert_statements(f: TextIO, gdf: GeoDataFrame, geom_type: str):
         f.write(
             f"INSERT INTO study_{geom_type} (asset_id, geom_quality_item_code, geom) VALUES ((SELECT asset_id FROM asset WHERE sgs_id = {row['IDSGS_neu']}), 'revised', ST_GeomFromText('{row['geometry'].wkt}', 2056));\n")
 
+def remove_interior_polygons(gdf: GeoDataFrame):
+    '''Remove interior polygons from a GeoDataFrame of polygons.'''
+    gdf['geometry'] = gdf['geometry'].apply(lambda x: Polygon(x.exterior.coords))
+    return gdf
+
 def main():
     path = os.path.join(filedir, filename)
     gdf_points = read_gdf(file=path, layer='backup_20240829__points', allowed_bemerkung=allowed_bemerkung, translation_dict=sgsid_translation_dict)
@@ -37,6 +44,7 @@ def main():
     gdf_polygons = read_gdf(file=path, layer='backup_20240829__polygons', allowed_bemerkung=allowed_bemerkung, translation_dict=sgsid_translation_dict)
     gdf_lines = gdf_lines.explode() # explode multi-linestrings to linestrings
     gdf_polygons = gdf_polygons.explode() # explode multi-polygons to polygons
+    gdf_polygons = remove_interior_polygons(gdf_polygons) # remove holes from polygons
     gdf_points = gdf_points[~gdf_points['geometry'].is_empty] # remove empty geometries
 
     sgs_ids = np.unique(np.concatenate(
