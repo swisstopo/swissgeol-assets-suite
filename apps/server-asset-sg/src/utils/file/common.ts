020695e8c540d5439bc5d15c802d6fc7f85adf00
Daniel von Atzigen <daniel@vatz.ch>
2025-01-08 14:31:40 +0100

    Make s3 credentials optional

--- apps/server-asset-sg/src/utils/file/common.ts
+++ apps/server-asset-sg/src/utils/file/common.ts
@@ -2,43 +2,53 @@ import { unknownToError } from '@asset-sg/core';
 import { S3Client } from '@aws-sdk/client-s3';
 import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
-import * as D from 'io-ts/Decoder';
 
-const AssetS3Config = D.struct({
-  s3ClientConfig: D.struct({
-    region: D.string,
-    endpoint: D.string,
-    credentials: D.struct({
-      accessKeyId: D.string,
-      secretAccessKey: D.string,
-    }),
-    forcePathStyle: D.boolean,
-  }),
-  bucketName: D.string,
-  assetFolder: D.string,
-});
-
-const maybeAssetS3Config = AssetS3Config.decode({
-  s3ClientConfig: {
-    region: process.env.S3_REGION,
-    endpoint: process.env.S3_ENDPOINT,
-    credentials: {
-      accessKeyId: process.env.S3_ACCESS_KEY_ID,
-      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    },
-
-    // Disables the client automatically appending the hostname to the bucket name.
-    forcePathStyle: true,
-  },
-  bucketName: process.env.S3_BUCKET_NAME,
-  assetFolder: process.env.S3_ASSET_FOLDER,
-});
-
-if (E.isLeft(maybeAssetS3Config)) {
-  throw new Error(`Invalid S3 config: ${D.draw(maybeAssetS3Config.left)}}`);
+interface S3ClientConfig {
+  region: string;
+  endpoint: string;
+  credentials?: {
+    accessKeyId: string;
+    secretAccessKey: string;
+  };
+  forcePathStyle: boolean;
 }
 
-export const { s3ClientConfig, bucketName, assetFolder } = maybeAssetS3Config.right;
+const maybeEnv = (name: string): string | undefined => {
+  const value = process.env[name];
+  return value == null || value.length === 0 ? undefined : value;
+};
+
+const env = (name: string): string => {
+  const value = maybeEnv(name);
+  if (value == null) {
+    console.error(`missing environment variable '${name}'`);
+    process.exit(1);
+  }
+  return value;
+};
+
+export const bucketName = env('S3_BUCKET_NAME');
+export const assetFolder = env('S3_ASSET_FOLDER');
+
+const s3ClientConfig: S3ClientConfig = {
+  region: env('S3_REGION'),
+  endpoint: env('S3_ENDPOINT'),
+
+  // Disables the client automatically appending the hostname to the bucket name.
+  forcePathStyle: true,
+};
+
+const credentials: Partial<S3ClientConfig['credentials']> = {
+  accessKeyId: maybeEnv('S3_ACCESS_KEY_ID'),
+  secretAccessKey: maybeEnv('S3_SECRET_ACCESS_KEY'),
+};
+
+if (credentials.accessKeyId != null && credentials.secretAccessKey != null) {
+  s3ClientConfig.credentials = credentials as S3ClientConfig['credentials'];
+} else if (credentials.accessKeyId != credentials.secretAccessKey) {
+  console.error(`Either both or none of 'S3_ACCESS_KEY_ID' and 'S3_SECRET_ACCESS_KEY' must be set.`);
+  process.exit(1);
+}
 
 export const createS3Client = () => TE.fromEither(E.tryCatch(() => new S3Client(s3ClientConfig), unknownToError));
 export const destroyS3Client = (s3Client: S3Client) => E.tryCatch(() => s3Client.destroy(), unknownToError);
