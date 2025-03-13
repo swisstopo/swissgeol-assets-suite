5d2b0573a7453d9586f0af9ca274bdffbdef5bd3
Lukas Merz <lukemerz@gmail.com>
2025-03-13 12:05:32 +0100

    Add toggle mechanism with mock data

--- libs/asset-viewer/src/lib/components/map-legend/map-legend.component.ts
+++ libs/asset-viewer/src/lib/components/map-legend/map-legend.component.ts
@@ -2,15 +2,74 @@ import { CdkAccordion, CdkAccordionItem } from '@angular/cdk/accordion';
 import { Component } from '@angular/core';
 import { ButtonComponent } from '@asset-sg/client-shared';
 import { SvgIconComponent } from '@ngneat/svg-icon';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { AsyncPipe } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+
+interface MapStyle {
+  name: string;
+  type: 'geometry' | 'access';
+  styleItems: MapStyleItem[];
+}
+
+interface MapStyleItem {
+  translationKey: string;
+  iconKey: string;
+}
+
+const mapStyles: MapStyle[] = [
+  {
+    name: 'Geometrie',
+    type: 'geometry',
+    styleItems: [
+      {
+        translationKey: 'Asset Punkt',
+        iconKey: 'arrow-up',
+      },
+      {
+        translationKey: 'Asset Linie',
+        iconKey: 'arrow-down',
+      },
+      {
+        translationKey: 'Asset Fläche',
+        iconKey: 'settings',
+      },
+    ],
+  },
+  {
+    name: 'Freigabe',
+    type: 'access',
+    styleItems: [
+      {
+        translationKey: 'Öffentlich',
+        iconKey: 'settings',
+      },
+      {
+        translationKey: 'Intern',
+        iconKey: 'arrow-up',
+      },
+      {
+        translationKey: 'gesperrt',
+        iconKey: 'arrow-down',
+      },
+    ],
+  },
+];
 
 @Component({
   selector: 'asset-sg-map-legend',
-  imports: [CdkAccordion, CdkAccordionItem, SvgIconComponent, ButtonComponent],
+  imports: [CdkAccordion, CdkAccordionItem, SvgIconComponent, ButtonComponent, AsyncPipe, TranslateModule],
   templateUrl: './map-legend.component.html',
   styleUrl: './map-legend.component.scss',
 })
 export class MapLegendComponent {
+  private activeStyleIndex = 0;
+  private activeStyleSubject = new BehaviorSubject(mapStyles[this.activeStyleIndex]);
+  protected activeStyle$ = this.activeStyleSubject.asObservable();
+
   protected handleChange() {
     // todo assets-300, assets-420: handle change
+    this.activeStyleIndex = (this.activeStyleIndex + 1) % mapStyles.length;
+    this.activeStyleSubject.next(mapStyles[this.activeStyleIndex]);
   }
 }
