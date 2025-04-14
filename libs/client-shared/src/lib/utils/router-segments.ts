dfeb0f61ace9787e44120a1e9cefdb0b1eaa3db6
till_schuetze <till.schuetze@ebp.ch>
2025-04-14 14:18:52 +0200

    create new injectionToken for retrieving the router-segments

---
+++ libs/client-shared/src/lib/utils/router-segments.ts
@@ -0,0 +1,19 @@
+import { inject, InjectionToken } from '@angular/core';
+import { NavigationEnd, Router, UrlSegment } from '@angular/router';
+import { filter, map, Observable, startWith } from 'rxjs';
+
+export const ROUTER_SEGMENTS = new InjectionToken<Observable<UrlSegment[] | undefined>>(
+  '@asset-sg/client-shared/router-segments'
+);
+
+export function routerSegmentsFactory(): Observable<UrlSegment[] | undefined> {
+  const router = inject(Router);
+  return router.events.pipe(
+    filter((event) => event instanceof NavigationEnd),
+    startWith(() => undefined),
+    map(() => {
+      return (router.getCurrentNavigation() ?? router.lastSuccessfulNavigation)?.finalUrl?.root.children?.['primary']
+        ?.segments;
+    })
+  );
+}
