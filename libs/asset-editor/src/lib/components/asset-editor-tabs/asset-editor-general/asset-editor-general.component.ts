70bcfbed088a24a58eaf3481663f42d3b20bb3ed
till_schuetze <till.schuetze@ebp.ch>
2025-04-16 15:06:23 +0200

    Fix issue with languageSelector by creating a new one from the ReferenceData

--- libs/asset-editor/src/lib/components/asset-editor-tabs/asset-editor-general/asset-editor-general.component.ts
+++ libs/asset-editor/src/lib/components/asset-editor-tabs/asset-editor-general/asset-editor-general.component.ts
@@ -1,13 +1,10 @@
 import { Component, inject, Input, OnDestroy, OnInit } from '@angular/core';
-import { fromAppShared, TranslatedValue, Translation } from '@asset-sg/client-shared';
+import { fromAppShared, TranslatedValue } from '@asset-sg/client-shared';
 import { ValueItem } from '@asset-sg/shared';
 import { Role, SimpleWorkgroup } from '@asset-sg/shared/v2';
 import { UntilDestroy } from '@ngneat/until-destroy';
 import { Store } from '@ngrx/store';
 import { combineLatestWith, map, Observable, startWith, Subscription } from 'rxjs';
-// TODO: move selector to different module so it can be used in all modules
-// eslint-disable-next-line @nx/enforce-module-boundaries
-import { selectLanguageFilters } from '../../../../../../asset-viewer/src/lib/state/asset-search/asset-search.selector';
 import { AssetForm } from '../../asset-editor-page/asset-editor-page.component';
 
 interface TranslatedValueItem {
@@ -27,7 +24,6 @@ export class AssetEditorGeneralComponent implements OnInit, OnDestroy {
   @Input() sgsId: number | null = null;
 
   public workgroups: SimpleWorkgroup[] = [];
-  public languages: { name: Translation; value: string }[] = [];
   public selectedLanguages: string[] = [];
   public selectedManCatLabels: TranslatedValueItem[] = [];
   public selectedNatRelItems: TranslatedValueItem[] = [];
@@ -35,57 +31,25 @@ export class AssetEditorGeneralComponent implements OnInit, OnDestroy {
   private readonly subscription: Subscription = new Subscription();
   public readonly natRelItems$: Observable<TranslatedValueItem[]> = this.store
     .select(fromAppShared.selectNatRelItems)
-    .pipe(
-      map((natRelItems) => {
-        if (natRelItems == null) {
-          return [];
-        }
-        return Object.values(natRelItems).map(translatedValueItemFromValueItem);
-      }),
-    );
+    .pipe(map(translatedValueItemFromValueItemRecord));
   public readonly manCatLabelItems$: Observable<TranslatedValueItem[]> = this.store
     .select(fromAppShared.selectManCatLabelItems)
-    .pipe(
-      map((manCatLabelItems) => {
-        if (manCatLabelItems == null) {
-          return [];
-        }
-        return Object.values(manCatLabelItems).map(translatedValueItemFromValueItem);
-      }),
-    );
+    .pipe(map(translatedValueItemFromValueItemRecord));
   public readonly assetFormatItems$: Observable<TranslatedValueItem[]> = this.store
     .select(fromAppShared.selectAssetFormatItems)
-    .pipe(
-      map((assetFormatItems) => {
-        if (assetFormatItems == null) {
-          return [];
-        }
-        return Object.values(assetFormatItems).map(translatedValueItemFromValueItem);
-      }),
-    );
+    .pipe(map(translatedValueItemFromValueItemRecord));
   public readonly assetKindItems$: Observable<TranslatedValueItem[]> = this.store
     .select(fromAppShared.selectAssetKindItems)
-    .pipe(
-      map((assetKindItems) => {
-        if (assetKindItems == null) {
-          return [];
-        }
-        return Object.values(assetKindItems).map(translatedValueItemFromValueItem);
-      }),
-    );
-  private readonly languageSelector$ = this.store.select(selectLanguageFilters).pipe(
-    map((languageFilters) => {
-      return languageFilters.map((lang) => ({ name: lang.name, value: lang.value }));
-    }),
-  );
-
+    .pipe(map(translatedValueItemFromValueItemRecord));
+  public readonly languageItems$: Observable<TranslatedValueItem[]> = this.store
+    .select(fromAppShared.selectLanguageItems)
+    .pipe(map(translatedValueItemFromValueItemRecord));
   public readonly availableWorkgroups$ = this.store
     .select(fromAppShared.selectWorkgroups)
     .pipe(map((workgroups) => workgroups.filter((it) => it.role != Role.Viewer)));
 
   public ngOnInit() {
     this.subscription.add(this.availableWorkgroups$.subscribe((workgroups) => (this.workgroups = workgroups)));
-    this.subscription.add(this.languageSelector$.subscribe((languages) => (this.languages = languages)));
     this.subscription.add(
       this.formGroup.controls.manCatLabelRefs.valueChanges
         .pipe(startWith(this.formGroup.controls.manCatLabelRefs.value), combineLatestWith(this.manCatLabelItems$))
@@ -100,8 +64,13 @@ export class AssetEditorGeneralComponent implements OnInit, OnDestroy {
           this.selectedNatRelItems = natRelItems.filter((item) => selectedValues?.includes(item.code));
         }),
     );
-    this.subscription.add(this.formGroup.valueChanges.subscribe(console.log));
-    this.selectedLanguages = this.formGroup.controls.assetLanguages.value?.map((lang) => lang.languageItemCode) ?? [];
+    this.subscription.add(
+      this.formGroup.controls.assetLanguages.valueChanges
+        .pipe(startWith(this.formGroup.controls.assetLanguages.value))
+        .subscribe((langs) => {
+          this.selectedLanguages = langs?.map((lang) => lang.languageItemCode) ?? [];
+        }),
+    );
   }
 
   public ngOnDestroy() {
@@ -113,12 +82,14 @@ export class AssetEditorGeneralComponent implements OnInit, OnDestroy {
       languageItemCode: l,
     }));
     this.formGroup.controls.assetLanguages.setValue(languages);
+    this.formGroup.markAsDirty();
   }
 
   public removeItemFromForm(item: TranslatedValueItem, controlName: 'manCatLabelRefs' | 'typeNatRels') {
     const currentValues = this.formGroup.controls[controlName].value ?? [];
     const newValues = currentValues.filter((value: string) => value !== item.code);
     this.formGroup.controls[controlName].setValue(newValues);
+    this.formGroup.markAsDirty();
   }
 
   public addNewAlternativeId() {
@@ -133,6 +104,13 @@ export class AssetEditorGeneralComponent implements OnInit, OnDestroy {
     this.formGroup.markAsDirty();
   }
 }
+
+const translatedValueItemFromValueItemRecord = (item: Record<string, ValueItem> | null): TranslatedValueItem[] => {
+  if (item == null) {
+    return [];
+  }
+  return Object.values(item).map(translatedValueItemFromValueItem);
+};
 
 const translatedValueItemFromValueItem = (item: ValueItem): TranslatedValueItem => {
   return {
