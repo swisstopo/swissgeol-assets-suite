c7d538f3cf2534d721a0eb894d0cf2bf8be76a5e
Daniel von Atzigen <daniel@vatz.ch>
2025-04-23 07:55:24 +0200

    Add `PATCH /assets/:assetId/workflow/review` endpoint

--- apps/server-asset-sg/src/features/assets/workflow/workflow.service.ts
+++ apps/server-asset-sg/src/features/assets/workflow/workflow.service.ts
@@ -1,14 +1,10 @@
-import { AssetId, UserId, Workflow, WorkflowChangeData, WorkflowStatus } from '@asset-sg/shared/v2';
+import { AssetId, UserId, Workflow, WorkflowChangeData, WorkflowSelection, WorkflowStatus } from '@asset-sg/shared/v2';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { WorkflowRepo } from '@/features/assets/workflow/workflow.repo';
-import { UserRepo } from '@/features/users/user.repo';
 
 @Injectable()
 export class WorkflowService {
-  constructor(
-    private readonly workflowRepo: WorkflowRepo,
-    private readonly userRepo: UserRepo,
-  ) {}
+  constructor(private readonly workflowRepo: WorkflowRepo) {}
 
   async find(assetId: AssetId): Promise<Workflow> {
     return handleMissing(await this.workflowRepo.find(assetId));
@@ -21,7 +17,7 @@ export class WorkflowService {
         HttpStatus.UNPROCESSABLE_ENTITY,
       );
     }
-    return this.workflowRepo.change(workflow.assetId, {
+    return this.workflowRepo.change(workflow.id, {
       creatorId,
       comment: change.comment,
       from: { status: workflow.status, assigneeId: workflow.assignee?.id ?? null },
@@ -29,11 +25,18 @@ export class WorkflowService {
     });
   }
 
+  async updateReview(workflow: Workflow, review: Partial<WorkflowSelection>): Promise<WorkflowSelection> {
+    if (workflow.status !== WorkflowStatus.InReview) {
+      throw new HttpException("Review can only be changed for workflows with 'InReview' status.", HttpStatus.CONFLICT);
+    }
+    return handleMissing(await this.workflowRepo.reviews.update(workflow.id, review));
+  }
+
   async publish(workflow: Workflow, creatorId: UserId) {
     if (workflow.status !== WorkflowStatus.Reviewed) {
       throw new HttpException('Cannot publish workflow in current status', HttpStatus.BAD_REQUEST);
     }
-    return this.workflowRepo.change(workflow.assetId, {
+    return this.workflowRepo.change(workflow.id, {
       creatorId: creatorId,
       comment: null,
       from: { status: workflow.status, assigneeId: workflow.assignee?.id ?? null },
