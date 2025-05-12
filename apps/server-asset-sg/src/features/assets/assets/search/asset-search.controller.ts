424e88abc7fa7d32380cfd20f837890783a5a3bb
Lukas Merz <lukemerz@gmail.com>
2025-05-12 11:43:41 +0200

    Use class-transformer for dto

--- apps/server-asset-sg/src/features/assets/assets/search/asset-search.controller.ts
+++ apps/server-asset-sg/src/features/assets/assets/search/asset-search.controller.ts
@@ -1,19 +1,29 @@
 import { AssetSearchQuery, AssetSearchQueryDTO, AssetSearchStats, AssetSearchStatsDTO } from '@asset-sg/shared';
-import { AssetSearchResult, AssetSearchResultDTO, User } from '@asset-sg/shared/v2';
-import { Controller, HttpCode, HttpStatus, Post, Query } from '@nestjs/common';
-import { plainToInstance } from 'class-transformer';
+import { AssetSearchResultDTO, User } from '@asset-sg/shared/v2';
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Query,
+  SerializeOptions,
+  UseInterceptors,
+} from '@nestjs/common';
 import { Authorize } from '@/core/decorators/authorize.decorator';
 import { CurrentUser } from '@/core/decorators/current-user.decorator';
 import { ParseBody } from '@/core/decorators/parse.decorator';
 import { AssetSearchService } from '@/features/assets/assets/search/asset-search.service';
 
 @Controller('/assets/search')
+@UseInterceptors(ClassSerializerInterceptor)
 export class AssetSearchController {
   constructor(private readonly assetSearchService: AssetSearchService) {}
 
   @Post('/')
   @Authorize.User()
   @HttpCode(HttpStatus.OK)
+  @SerializeOptions({ type: AssetSearchResultDTO, excludeExtraneousValues: true })
   async search(
     @ParseBody(AssetSearchQueryDTO)
     query: AssetSearchQuery,
@@ -22,25 +32,24 @@ export class AssetSearchController {
     limit?: number,
     @Query('offset')
     offset?: number,
-  ): Promise<AssetSearchResult> {
+  ): Promise<AssetSearchResultDTO> {
     limit = limit == null ? limit : Number(limit);
     offset = offset == null ? offset : Number(offset);
     restrictQueryForUser(query, user);
-    const result = await this.assetSearchService.search(query, user, { limit, offset, decode: false });
-    return plainToInstance(AssetSearchResultDTO, result);
+    return await this.assetSearchService.search(query, user, { limit, offset, decode: false });
   }
 
   @Post('/stats')
   @Authorize.User()
   @HttpCode(HttpStatus.OK)
+  @SerializeOptions({ type: AssetSearchStatsDTO })
   async showStats(
     @ParseBody(AssetSearchQueryDTO)
     query: AssetSearchQuery,
     @CurrentUser() user: User,
   ): Promise<AssetSearchStats> {
     restrictQueryForUser(query, user);
-    const stats = await this.assetSearchService.aggregate(query, user);
-    return plainToInstance(AssetSearchStatsDTO, stats);
+    return await this.assetSearchService.aggregate(query, user);
   }
 }
 
