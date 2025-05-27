7c55581d6a2d6c8658707b2b4eb3106c0eed34e1
Lukas Merz <lukemerz@gmail.com>
2025-05-27 16:02:11 +0200

    WIP

--- apps/sync-asset-sg/src/core/export-to-view.service.ts
+++ apps/sync-asset-sg/src/core/export-to-view.service.ts
@@ -2,15 +2,58 @@ import { Prisma, PrismaClient } from '@prisma/client';
 import { SyncConfig } from './config';
 import { log } from './log';
 
+type PrismaAssetSelection = Prisma.AssetGetPayload<{
+  select: {
+    assetId: true;
+    titlePublic: true;
+    isExtract: true;
+    createDate: true;
+    lastProcessedDate: true;
+    receiptDate: true;
+    assetContacts: true;
+    assetFiles: {
+      include: {
+        file: true;
+      };
+    };
+    isNatRel: true;
+    assetKindItemCode: true;
+    assetFormatItemCode: true;
+    workgroupId: true;
+    isPublic: true;
+    studyAreas: true;
+    studyLocations: true;
+    studyTraces: true;
+    assetMainId: true;
+    sgsId: true;
+    geolDataInfo: true;
+    geolAuxDataInfo: true;
+    geolContactDataInfo: true;
+    municipality: true;
+  };
+}>;
+
 interface AssetInfo {
   assetId: number;
   workgroupId: number;
+  publishData: {
+    authors: boolean;
+    initiators: boolean;
+    suppliers: boolean;
+    general: boolean;
+    geometries: boolean;
+    legacy: boolean;
+    normalFiles: boolean;
+    legalFiles: boolean;
+    references: boolean;
+  };
 }
 
 const BATCH_SIZE = 500;
 
 export class ExportToViewService {
   private readonly allowedWorkgroupIds: number[];
+  private readonly publicAssetConfigs: Map<number, Omit<AssetInfo, 'assetId'>> = new Map();
 
   private readonly sourcePrisma: PrismaClient;
   private readonly destinationPrisma: PrismaClient;
@@ -23,8 +66,9 @@ export class ExportToViewService {
 
   public async exportToView() {
     const publicAssets = await this.findPublicAssetIds();
-    const publicAssetIds = publicAssets.map((item) => item.assetId);
-    log(`Found ${publicAssetIds.length} public assets.`);
+    publicAssets.forEach(({ assetId, ...rest }) => this.publicAssetConfigs.set(assetId, rest));
+
+    log(`Found ${this.publicAssetConfigs.size} public assets.`);
 
     const batches = this.batchList(publicAssets);
     log(`Created ${batches.length} batches with batch size ${BATCH_SIZE}.`);
@@ -42,9 +86,6 @@ export class ExportToViewService {
       await this.export('assetLanguage', 'assetId', assetIds);
 
       await this.export('manCatLabelRef', 'assetId', assetIds);
-      await this.exportStudyAreas(assetIds);
-      await this.exportStudyLocations(assetIds);
-      await this.exportStudyTraces(assetIds);
       await this.export('typeNatRel', 'assetId', assetIds);
 
       const timeTaken = Date.now() - time;
@@ -54,109 +95,15 @@ export class ExportToViewService {
     // only export siblings after all assets have been exported so no foreign key constraint is violated
     for (const batch of batches) {
       const assetIds = batch.map((item) => item.assetId);
-      await this.exportSiblings(assetIds, publicAssetIds);
+      await this.exportSiblings(assetIds, [...this.publicAssetConfigs.keys()]);
     }
-
-    await this.exportFiles(publicAssetIds);
-  }
-
-  /**
-   * Export study areas as raw query with st_astext.
-   */
-  private async exportStudyAreas(assetIds: number[]) {
-    const studyAreas = await this.sourcePrisma.$queryRaw<{ studyAreaId: number; assetId: number; geom: string }[]>`
-      SELECT study_area_id          as "studyAreaId",
-             asset_id               as "assetId",
-             st_astext(geom, 2056)  as geom
-      FROM study_area
-      WHERE asset_id IN (${Prisma.join(assetIds)})
-    `;
-
-    if (studyAreas.length === 0) {
-      log(`No study areas found. Continuing.`);
-      return;
-    }
-
-    const formattedStudyAreas = studyAreas.map(
-      (sa) => Prisma.sql`(${sa.studyAreaId}, ${sa.assetId}, ST_GeomFromText(${sa.geom}, 2056))`,
-    );
-
-    // insert into destination as raw query
-    const result = await this.destinationPrisma.$executeRaw`
-      INSERT INTO study_area (study_area_id, asset_id, geom)
-      VALUES ${Prisma.join(formattedStudyAreas)}
-    `;
-
-    log(`Created ${result} study areas.`);
-  }
-
-  /**
-   * Export study locations as raw query with st_astext.
-   */
-  private async exportStudyLocations(assetIds: number[]) {
-    const studyLocations = await this.sourcePrisma.$queryRaw<
-      { studyLocationId: number; assetId: number; geom: string }[]
-    >`
-      SELECT study_location_id      as "studyLocationId",
-             asset_id               as "assetId",
-             st_astext(geom, 2056)  as geom
-      FROM study_location
-      WHERE asset_id IN (${Prisma.join(assetIds)})
-    `;
-
-    if (studyLocations.length === 0) {
-      log(`No study locations found. Continuing.`);
-      return;
-    }
-
-    const formattedStudyLocations = studyLocations.map(
-      (sa) => Prisma.sql`(${sa.studyLocationId}, ${sa.assetId}, ST_GeomFromText(${sa.geom}, 2056))`,
-    );
-
-    // insert into destination as raw query
-    const result = await this.destinationPrisma.$executeRaw`
-      INSERT INTO study_location (study_location_id, asset_id, geom)
-      VALUES ${Prisma.join(formattedStudyLocations)}
-    `;
-
-    log(`Created ${result} study locations.`);
-  }
-
-  /**
-   * Export study traces as raw query with st_astext.
-   */
-  private async exportStudyTraces(assetIds: number[]) {
-    const studyTraces = await this.sourcePrisma.$queryRaw<{ studyTraceId: number; assetId: number; geom: string }[]>`
-      SELECT study_trace_id         as "studyTraceId",
-             asset_id               as "assetId",
-             st_astext(geom, 2056)  as geom
-      FROM study_trace
-      WHERE asset_id IN (${Prisma.join(assetIds)})
-    `;
-
-    if (studyTraces.length === 0) {
-      log(`No study traces found. Continuing.`);
-      return;
-    }
-
-    const formattedStudyTraces = studyTraces.map(
-      (sa) => Prisma.sql`(${sa.studyTraceId}, ${sa.assetId}, ST_GeomFromText(${sa.geom}, 2056))`,
-    );
-
-    // insert into destination as raw query
-    const result = await this.destinationPrisma.$executeRaw`
-      INSERT INTO study_trace (study_trace_id, asset_id, geom)
-      VALUES ${Prisma.join(formattedStudyTraces)}
-    `;
-
-    log(`Created ${result} study traces.`);
   }
 
   /**
    * Export assets with the given ids.
    */
   private async exportAssets(assetIds: number[]) {
-    const assets = await this.sourcePrisma.asset.findMany({
+    const assets: PrismaAssetSelection[] = await this.sourcePrisma.asset.findMany({
       where: {
         assetId: {
           in: assetIds,
@@ -165,35 +112,53 @@ export class ExportToViewService {
       select: {
         assetId: true,
         titlePublic: true,
-        isNatRel: true,
-        receiptDate: true,
-        url: true,
-        locationAnalog: true,
-        lastProcessedDate: true,
-        textBody: true,
-        remark: true,
-        assetKindItemCode: true,
-        createDate: true,
-        assetFormatItemCode: true,
-        authorBiblio: true,
-        sourceProject: true,
-        description: true,
         isExtract: true,
-        assetMainId: true,
+        createDate: true,
+        lastProcessedDate: true,
+        receiptDate: true,
+        assetContacts: true,
+        assetFiles: {
+          include: { file: true },
+        },
+        isNatRel: true,
+        assetKindItemCode: true,
+        assetFormatItemCode: true,
         workgroupId: true,
-        titleOriginal: false,
-        sgsId: false,
-        geolDataInfo: false,
-        geolContactDataInfo: false,
-        geolAuxDataInfo: false,
-        municipality: false,
-        processor: false,
+        isPublic: true,
+        studyAreas: true,
+        studyTraces: true,
+        studyLocations: true,
+        assetMainId: true,
+        sgsId: true,
+        geolDataInfo: true,
+        geolAuxDataInfo: true,
+        geolContactDataInfo: true,
+        municipality: true,
       },
     });
+    const filteredAssets = this.preparePublishedData(assets);
 
-    const result = await this.destinationPrisma.asset.createMany({ data: assets });
-
+    let result = await this.destinationPrisma.asset.createMany({ data: filteredAssets.assets });
     log(`Created ${result.count} assets.`);
+
+    result = await this.destinationPrisma.assetContact.createMany({ data: filteredAssets.assetContacts });
+    log(`Created ${result.count} assetContacts.`);
+
+    result = await this.destinationPrisma.assetFile.createMany({ data: filteredAssets.assetFiles });
+    log(`Created ${result.count} assetFiles.`);
+
+    result = await this.destinationPrisma.studyLocation.createMany({ data: filteredAssets.geometries.locations });
+    log(`Created ${result.count} studyLocations.`);
+
+    result = await this.destinationPrisma.studyArea.createMany({ data: filteredAssets.geometries.areas });
+    log(`Created ${result.count} studyAreas.`);
+
+    result = await this.destinationPrisma.studyTrace.createMany({ data: filteredAssets.geometries.traces });
+    log(`Created ${result.count} studyTraces.`);
+
+    // todo: skip
+    //result = await this.destinationPrisma.assetXAssetY.createMany({ data: filteredAssets.siblings });
+    //log(`Created ${result.count} siblings.`);
   }
 
   /**
@@ -208,6 +173,8 @@ export class ExportToViewService {
       'LegalDocItem',
       'ManCatLabelItem',
       'NatRelItem',
+      'Contact',
+      'File',
     ];
 
     for (const table of tables) {
@@ -236,28 +203,6 @@ export class ExportToViewService {
   }
 
   /**
-   * Export non-legal files.
-   */
-  private async exportFiles(assetIds: number[]) {
-    log(`Starting file export.`);
-    // Read all unique file ids from the assetFiles which are not legal docs.
-    const assetFiles = await this.sourcePrisma.assetFile.findMany({
-      where: { assetId: { in: assetIds }, file: { type: { not: 'Legal' } } },
-    });
-    const fileIds = [...new Set(assetFiles.map((af) => af.fileId))];
-    const files = await this.sourcePrisma.file.findMany({ where: { id: { in: fileIds } } });
-
-    // Write files to the destination database
-    const fileResult = await this.destinationPrisma.file.createMany({
-      data: files,
-      skipDuplicates: true,
-    });
-    log(`Created ${fileResult.count} files.`);
-    const assetFileResult = await this.destinationPrisma.assetFile.createMany({ data: assetFiles });
-    log(`Created ${assetFileResult.count} AssetFiles.`);
-  }
-
-  /**
    * Export siblings.
    */
   private async exportSiblings(ids: number[], allPublicAssetIds: number[]) {
@@ -274,10 +219,22 @@ export class ExportToViewService {
    * Find public asset ids.
    */
   private async findPublicAssetIds() {
-    return this.sourcePrisma.$queryRaw<AssetInfo[]>`SELECT a.asset_id        as "assetId",
-                                                           a.workgroup_id    as "workgroupId"
+    return this.sourcePrisma.$queryRaw<AssetInfo[]>`SELECT a.asset_id     AS "assetId",
+                                                           a.workgroup_id AS "workgroupId",
+                                                           json_build_object(
+                                                             'authors', ws.authors,
+                                                             'initiators', ws.initiators,
+                                                             'suppliers', ws.suppliers,
+                                                             'general', ws.general,
+                                                             'geometries', ws.geometries,
+                                                             'legacy', ws.legacy,
+                                                             'normalFiles', ws.normal_files,
+                                                             'legalFiles', ws.legal_files,
+                                                             'references', ws."references"
+                                                           )              as "publishData"
                                                     FROM asset a
                                                            LEFT JOIN workflow w ON a.asset_id = w.id
+                                                           LEFT JOIN workflow_selection ws ON w.approval_id = ws.id
                                                     WHERE a.is_public
                                                       AND w.status = 'Published'
                                                       AND workgroup_id IN (${Prisma.join(this.allowedWorkgroupIds)})
@@ -295,4 +252,71 @@ export class ExportToViewService {
     }
     return batches;
   }
+
+  private preparePublishedData(assets: PrismaAssetSelection[]): {
+    assets: Prisma.AssetCreateManyInput[];
+    assetContacts: Prisma.AssetContactCreateManyInput[];
+    assetFiles: Prisma.AssetFileCreateManyInput[];
+    geometries: GeometryContainer;
+  } {
+    const filteredAssets: Prisma.AssetCreateManyInput[] = [];
+    const filteredAssetFiles: Prisma.AssetFileCreateManyInput[] = [];
+    const filteredAssetContacts: Prisma.AssetContactCreateManyInput[] = [];
+    const filteredGeometries: GeometryContainer = { areas: [], traces: [], locations: [] };
+    for (const { assetFiles, assetContacts, studyTraces, studyAreas, studyLocations, ...asset } of assets) {
+      const { publishData } = this.publicAssetConfigs.get(asset.assetId);
+      if (publishData.geometries) {
+        filteredGeometries.areas.push(...studyAreas);
+        filteredGeometries.locations.push(...studyLocations);
+        filteredGeometries.traces.push(...studyTraces);
+      }
+      // todo lme: fix module boundaries for ContactAssignmentRole
+      if (publishData.authors) {
+        filteredAssetContacts.push(...assetContacts.filter((c) => c.role === 'author'));
+      }
+      if (publishData.initiators) {
+        filteredAssetContacts.push(...assetContacts.filter((c) => c.role === 'initiator'));
+      }
+      if (publishData.suppliers) {
+        filteredAssetContacts.push(...assetContacts.filter((c) => c.role === 'supplier'));
+      }
+
+      if (publishData.legalFiles) {
+        filteredAssetFiles.push(
+          ...assetFiles.filter((f) => f.file.type === 'Legal').map((f) => ({ assetId: f.assetId, fileId: f.fileId })),
+        );
+      }
+      if (publishData.normalFiles) {
+        filteredAssetFiles.push(
+          ...assetFiles.filter((f) => f.file.type === 'Normal').map((f) => ({ assetId: f.assetId, fileId: f.fileId })),
+        );
+      }
+
+      // todo: make general stuff optional -> currently not possible
+      const filteredAsset: Prisma.AssetCreateManyInput = {
+        ...asset,
+        // optional fields for siblings
+        assetMainId: publishData.references ? asset.assetMainId : null,
+        // optional fields for legacy
+        sgsId: publishData.legacy ? asset.sgsId : null,
+        geolDataInfo: publishData.legacy ? asset.geolDataInfo : null,
+        geolContactDataInfo: publishData.legacy ? asset.geolContactDataInfo : null,
+        geolAuxDataInfo: publishData.legacy ? asset.geolAuxDataInfo : null,
+        municipality: publishData.legacy ? asset.municipality : null,
+      };
+      filteredAssets.push(filteredAsset);
+    }
+    return {
+      assets: filteredAssets,
+      assetContacts: filteredAssetContacts,
+      assetFiles: filteredAssetFiles,
+      geometries: filteredGeometries,
+    };
+  }
 }
+
+type GeometryContainer = {
+  traces: Prisma.StudyTraceCreateManyInput[];
+  areas: Prisma.StudyAreaCreateManyInput[];
+  locations: Prisma.StudyLocationCreateManyInput[];
+};
