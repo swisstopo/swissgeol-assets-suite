50f267f1ed2ac42ec49a07949a4a831c54369a1f
Lukas Merz <lukemerz@gmail.com>
2025-06-04 10:20:27 +0200

    Remove scripts folder

--- libs/persistence/prisma/scripts/shape_file_import/import_study_trace.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-
-ogr2ogr -nln study_trace -lco GEOMETRY_NAME=geom -lco FID=id -lco PRECISION=NO Pg:"dbname=postgres host=postgres user=postgres port=5432 password=postgres" study_trace.shp
-
