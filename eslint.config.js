c6952f3a7dbd62e16b929b288946572b1fb4b005
Daniel von Atzigen <daniel@vatz.ch>
2025-08-13 09:40:45 +0200

    Enable `@typescript-eslint/no-non-null-assertion` eslint rule

--- eslint.config.js
+++ eslint.config.js
@@ -89,7 +89,6 @@ module.exports = [
       'sort-imports': 'off',
 
       '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-non-null-assertion': 'off',
       'unused-imports/no-unused-imports': 'error',
       'unused-imports/no-unused-vars': [
         'warn',
