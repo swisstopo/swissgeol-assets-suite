73b4c876c76ebe6306d1a17e326138cd27d123cd
Lukas Merz <lukemerz@gmail.com>
2025-09-01 09:11:31 +0200

    Fix broken translate pipe

--- libs/asset-viewer/src/lib/components/asset-viewer-files/file-metadata.pipe.ts
+++ libs/asset-viewer/src/lib/components/asset-viewer-files/file-metadata.pipe.ts
@@ -1,7 +1,7 @@
 import { DecimalPipe } from '@angular/common';
 import { inject, Pipe, PipeTransform } from '@angular/core';
 import { AssetFile } from '@asset-sg/shared/v2';
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Pipe({
   name: 'fileMetadata',
@@ -10,7 +10,7 @@ import { TranslatePipe } from '@ngx-translate/core';
 })
 export class FileMetadataPipe implements PipeTransform {
   private readonly decimalPipe = inject(DecimalPipe);
-  private readonly translatePipe = inject(TranslatePipe);
+  private readonly translateService = inject(TranslateService);
 
   transform({ size, pageCount }: AssetFile, locale: string): string {
     const _fileSize = Math.round((size / 1024 / 1024) * 10) / 10;
@@ -23,8 +23,8 @@ export class FileMetadataPipe implements PipeTransform {
 
     const pageCountDisplay =
       pageCount > 1
-        ? this.translatePipe.transform('search.filePagePlural', { pageCount })
-        : this.translatePipe.transform('search.filePageSingular');
+        ? this.translateService.instant('search.filePagePlural', { pageCount })
+        : this.translateService.instant('search.filePageSingular');
 
     return [formattedFileSize, pageCountDisplay].join(', ');
   }
